63d1ae460acc17d5d66218f55911c4f7fe0e5c0e
filipemleite94 <filipemleite94@gmail.com>
2016-04-20 00:50:05 -0300

    Added files via upload
    Adicionado comentários a classe Valor

--- Calculadora/src/Valor.java
+++ Calculadora/src/Valor.java
@@ -1,4 +1,6 @@
+//Essa classe vai ser onde acontece de verdade as operações da calculadora.
 public class Valor {
+	//inicialização das variáveis que vão ser utilizadas.
 	protected double valor1, valor2;
 	protected char operando2;
 	public Valor(){
@@ -6,6 +8,8 @@ public class Valor {
 		valor2=0;
 		operando2='+';
 	}
+	
+	//Operações a ser realizadas
 	public void operacoes(char operando){
 		if(operando2=='+'){
 			valor1=valor1+valor2;
@@ -22,9 +26,14 @@ public class Valor {
 		if (operando=='='){
 			System.out.print(valor1);
 		}
+		
+		//Uma vez guardado o resultado da operação passada no valor1 reinicia-se a variável valor 2 e prepara-se
+		//o operando pra receber uma nova operação.
 		valor2=0;
 		operando2=operando;
 	}
+	
+	//Aqui o valor que está entre operandos é armazenado em valor 2
 	public void read(double valor){
 		if(operando2=='='){
 			valor1=0;
@@ -32,6 +41,8 @@ public class Valor {
 		}
 		valor2=10*valor2+valor;
 	}
+	
+	//A calculadora é reniciada
 	public void clear(){
 		valor1=0;
 		valor2=0;
